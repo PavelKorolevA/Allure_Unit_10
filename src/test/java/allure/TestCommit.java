6bfb8c1ab8981388f2de4590cbf616e7220274ec
p.korolev <amlerok96@gmail.com>
2022-04-07 23:19:53 +0300

    added class TestCommit

---
+++ src/test/java/allure/TestCommit.java
@@ -0,0 +1,13 @@
+package allure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestCommit {
+
+    @Test
+    void testAssertTrue() {
+        assertTrue(6>1);
+    }
+}
